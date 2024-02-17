74db66973ea71f09ab4b9986671980e3f9aab4e7
PaulDebril <debril.paul@gmail.com>
2024-02-17 11:39:49 +0100

    add content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from "./about/about.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, NavbarComponent, AboutComponent, SkillsComponent, ProjectsComponent, FooterComponent, ContactComponent]
 })
 export class AppComponent {
   title = 'Portfolio';
