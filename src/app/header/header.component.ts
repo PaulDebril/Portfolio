bf2ee9438acdef336928d9b548692c77344fd7bb
PaulDebril <debril.paul@gmail.com>
2024-04-15 22:50:40 +0200

    add fontawesome

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 }
